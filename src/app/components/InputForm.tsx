a49faba5169ff716117497e0a91d4a1d2c7d4f83
Jihah <sitinajihah.jainis@cimb.com>
2024-09-27 00:37:45 +0800

    change border color

--- src/app/components/InputForm.tsx
+++ src/app/components/InputForm.tsx
@@ -8,8 +8,9 @@ interface InputFormProps {
   const InputForm: React.FC<InputFormProps> = ({ label, value, onChange }) => {
     return (
       <div>
-        <label>{label}</label>
-        <input type="number" value={value} onChange={(e) => onChange(parseFloat(e.target.value))} />
+        <label className="block text-sm font-medium leading-6">{label}</label>
+        <input className="block flex-1 border border-white bg-transparent py-1.5 pl-1 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            type="number" value={value} onChange={(e) => onChange(parseFloat(e.target.value))} />
       </div>
     );
   };
